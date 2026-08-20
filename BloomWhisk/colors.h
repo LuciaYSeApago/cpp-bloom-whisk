96d3253bb53165d18b2ba69af02c2c52dcc6e109
starrymoonzip <lucialvarez2003@gmail.com>
2026-08-20 19:40:30 +0200

    🎨 Add main window skeleton

--- BloomWhisk/colors.h
+++ BloomWhisk/colors.h
@@ -5,9 +5,9 @@
 namespace BloomColors
 {
     const QString Cream = "#FFF8F0";
-    const QString Pink = "#FDA2BC";
-    const QString LightPink = "#FFC4D4";
+    const QString Sakura = "#FDA2BC";
+    const QString Pink = "#FFC4D4";
     const QString Matcha = "#A9DD82";
-    const QString LightGreen = "#C4EDAB";
+    const QString Green = "#C4EDAB";
     const QString Dark = "#3D3542";
 }
