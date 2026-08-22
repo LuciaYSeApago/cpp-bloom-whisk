590410283e4f896281bb69ae60c660862af9d551
starrymoonzip <lucialvarez2003@gmail.com>
2026-08-22 12:50:51 +0200

    🌸 Add data persistance within the app

--- BloomWhisk/mainwindow.cpp
+++ BloomWhisk/mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "colors.h"
+#include "matcha.h"
+
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     //conects window with whatever is designed in ui
     ui->setupUi(this);
+
+    matcha sayaka(
+        "Ippodo Sayaka",
+        "Smooth, creamy",
+        5,
+        12
+        );
+
+    qDebug()<< sayaka.getName();
+    qDebug()<< sayaka.getInfo();
+    qDebug()<< sayaka.getRating();
+    qDebug()<< sayaka.getTimesConsumed();
 
     setWindowTitle("BloomWhisk");
 
